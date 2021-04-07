946bb5257d2ae525392eea51b76a32fd35bc24b3
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-04-07 23:47:11 +1000

    Add stackdriver-agent

--- deploy/vm/startup_script_prod.sh
+++ deploy/vm/startup_script_prod.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+set -x
+
+# gcsfuse
 echo "deb http://packages.cloud.google.com/apt gcsfuse-focal main" | tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+# stackdriver-agent
+curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
+bash add-monitoring-agent-repo.sh
 
 apt update
 apt install -y gcsfuse docker.io
 
+service stackdriver-agent start
+
+# Mount GCS bucket for static file serving.
 mkdir -p /seqr_static_files
 gcsfuse cpg-seqr-static-files /seqr_static_files
 
 gcloud -q auth configure-docker australia-southeast1-docker.pkg.dev
 
-docker system prune -f -a   # Clear Docker cache on restarts.
+# Clear Docker cache, in case the VM restarted.
+docker system prune -f -a
 
 docker run --name seqr -p 80:8000 \
     -v /seqr_static_files:/seqr_static_files \
@@ -30,7 +41,7 @@ docker run --name seqr -p 80:8000 \
     -e KIBANA_SERVICE_PORT=9243 \
     -e KIBANA_ES_PASSWORD=sm://seqr-308602/kibana-es-password \
     -e REDIS_SERVICE_HOSTNAME=10.94.144.3 \
-    -e GUNICORN_WORKER_THREADS=4 \
+    -e GUNICORN_WORKER_THREADS=20 \
     -e SOCIAL_AUTH_GOOGLE_OAUTH2_CLIENT_ID=1021400127367-9uc4sikfsm0vqo38q1g6rclj91mm501r.apps.googleusercontent.com \
     -e SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET=sm://seqr-308602/social-auth-google-oauth2-secret-prod \
     -e DJANGO_KEY=sm://seqr-308602/django-key-prod \
