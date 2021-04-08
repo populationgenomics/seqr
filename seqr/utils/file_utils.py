d44d26444e847a78138a29771158b6ceffb22af1
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-04-08 21:35:26 +1000

    Don't get blob size, as that incurs an RPC

--- seqr/utils/file_utils.py
+++ seqr/utils/file_utils.py
@@ -8,6 +8,8 @@ import google.cloud.storage
 logger = logging.getLogger(__name__)
 storage_client = None
 
+ANVIL_BUCKET_PREFIX = 'fc-secure'
+ANVIL_BILLING_PROJECT = 'anvil-datastorage'
 
 def _gcs_storage_client():
     """Returns a lazily initialized GCS storage client."""
@@ -19,7 +21,7 @@ def _gcs_storage_client():
 
 def _run_gsutil_command(command, gs_path, gunzip=False):
     #  Anvil buckets are requester-pays and we bill them to the anvil project
-    project_arg = '-u anvil-datastorage ' if gs_path.startswith('gs://fc-secure') else ''
+    project_arg = f'-u {ANVIL_BILLING_PROJECT} ' if gs_path.startswith(f'gs://{ANVIL_BUCKET_PREFIX}') else ''
     command = 'gsutil {project_arg}{command} {gs_path}'.format(
         project_arg=project_arg, command=command, gs_path=gs_path,
     )
@@ -46,19 +48,20 @@ def file_iter(file_path, byte_range=None, raw_content=False):
         path_segments = file_path.split('/')
         if len(path_segments) < 4:
             raise ValueError(f'Invalid GCS path: "{file_path}"')
-        bucket = _gcs_storage_client().bucket(path_segments[2])
+        user_project = ANVIL_BILLING_PROJECT if path_segments[2].startswith(ANVIL_BUCKET_PREFIX) else None
+        bucket = _gcs_storage_client().bucket(path_segments[2], user_project)
         blob = bucket.blob('/'.join(path_segments[3:]))
         current = byte_range[0] if byte_range else 0
-        end = byte_range[1] if byte_range else blob.size()
+        end = byte_range[1] if byte_range else None
         while True:
-            next = min(current + (1 << 20), end)  # 1 MB chunks
-            if current >= next:
+            next = current + (1 << 20)  # 1 MB chunks
+            if end and end < next:
+                next = end
+            data = blob.download_as_bytes(start=current, end=next)
+            current += len(data)
+            yield data if raw_content else data.decode('utf-8')
+            if current < next or current == end:
                 break
-            if raw_content:
-                yield blob.download_as_bytes(start=current, end=next)
-            else:
-                yield blob.download_as_string(start=current, end=next)
-            current = next 
     else:
         mode = 'rb' if raw_content else 'r'
         with open(file_path, mode) as f:
