3e2bbacb80191a12da407094f0d2f724d6721857
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-04-08 18:59:00 +1000

    Simplify

--- seqr/utils/file_utils.py
+++ seqr/utils/file_utils.py
@@ -37,39 +37,31 @@ def does_file_exist(file_path):
     return os.path.isfile(file_path)
 
 
-def file_iter(file_path, byte_range=None, raw_content=False):
-    if _is_google_bucket_file_path(file_path):
-        for line in _google_bucket_file_iter(file_path, byte_range=byte_range, raw_content=raw_content):
-            yield line
-    else:
-        mode = 'rb' if raw_content else 'r'
-        with open(file_path, mode) as f:
-            if byte_range:
-                f.seek(byte_range[0])
-                for line in f:
-                    if f.tell() < byte_range[1]:
-                        yield line
-                    else:
-                        break
+def _iterate(f, byte_range=None):
+    if byte_range:
+        f.seek(byte_range[0])
+        for line in f:
+            if f.tell() < byte_range[1]:
+                yield line
             else:
-                for line in f:
-                    yield line
+                break
+    else:
+        for line in f:
+            yield line
 
 
-def _google_bucket_file_iter(gs_path, byte_range=None, raw_content=False):
-    path_segments = gs_path.split('/')
-    if len(path_segments) < 4:
-        raise ValueError(f'Invalid GCS path: "{gs_path}"')
-    bucket = _gcs_storage_client().bucket(path_segments[2])
-    blob = bucket.blob('/'.join(path_segments[3:]))
-    with blob.open(mode='rb' if raw_content else 'r') as f:
-        if byte_range:
-            f.seek(byte_range[0])
-            for line in f:
-                if f.tell() < byte_range[1]:
-                    yield line
-                else:
-                    break
-        else:
-            for line in f:
+def file_iter(file_path, byte_range=None, raw_content=False):
+    mode = 'rb' if raw_content else 'r'
+    if _is_google_bucket_file_path(file_path):
+        path_segments = file_path.split('/')
+        if len(path_segments) < 4:
+            raise ValueError(f'Invalid GCS path: "{file_path}"')
+        bucket = _gcs_storage_client().bucket(path_segments[2])
+        blob = bucket.blob('/'.join(path_segments[3:]))
+        with blob.open(mode) as f:
+            for line in _iterate(f, byte_range):
+                yield line
+    else:
+        with open(file_path, mode) as f:
+            for line in _iterate(f, byte_range):
                 yield line
