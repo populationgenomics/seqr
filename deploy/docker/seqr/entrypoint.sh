109ae98232158b0bc7263fc8d521fc6ce9622335
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-04-14 13:15:10 +1000

    Rename /opt -> /app

--- deploy/docker/seqr/entrypoint.sh
+++ deploy/docker/seqr/entrypoint.sh
@@ -7,4 +7,4 @@ rm /tmp/gsa-key.json
 # link to persistent disk dir with static files
 mkdir -p /seqr_static_files/generated_files
 
-gunicorn -w $GUNICORN_WORKER_THREADS -c /opt/seqr/gunicorn_config.py wsgi:application
+gunicorn -w $GUNICORN_WORKER_THREADS -c /app/seqr/gunicorn_config.py wsgi:application
