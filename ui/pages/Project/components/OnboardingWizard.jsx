0bab2db5f3eac59aa0df262f795e0b45c183834d
daniaki <7043686+daniaki@users.noreply.github.com>
2021-09-09 17:54:44 +1000

    Stub

---
+++ ui/pages/Project/components/OnboardingWizard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+// import { connect } from 'react-redux'
+
+
+const OnboardingWizard = () => {
+  return <div>Hello world!</div>
+}
+
+OnboardingWizard.propTypes = {}
+
+export default OnboardingWizard
