ddf9b3e271533059a9af870bf9eebe1bc9d62808
daniaki <7043686+daniaki@users.noreply.github.com>
2021-10-29 12:30:45 +1100

    Serve 401 if user doesn't have edit permissions

--- ui/pages/Project/components/OnboardingWizard/OnboardingWizard.jsx
+++ ui/pages/Project/components/OnboardingWizard/OnboardingWizard.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
+import { Error401 } from '../../../../shared/components/page/Errors'
 import { getCurrentProject } from '../../selectors'
 
 const OnboardingWizard = ({ match, project }) => {
+  if (!project.canEdit) {
+    return <Error401 />
+  }
+
   return (
     <div>
       <h1>Hello, world!</h1>
-      <pre>{ JSON.stringify(match, null, 2) }</pre>
-      <pre>{ JSON.stringify(project, null, 2) }</pre>
+      <pre>{JSON.stringify(match, null, 2)}</pre>
+      <pre>{JSON.stringify(project, null, 2)}</pre>
     </div>
   )
 }
