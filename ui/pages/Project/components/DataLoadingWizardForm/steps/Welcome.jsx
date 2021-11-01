b4b8e7bbbe176bb9365904f51f434b56f14f4431
daniaki <7043686+daniaki@users.noreply.github.com>
2021-11-01 17:21:57 +1100

    Welcome step

---
+++ ui/pages/Project/components/DataLoadingWizardForm/steps/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <fragment>
+      <p>
+        Welcome to the seqr data loading wizard! This wizard will guide you through the process of uploading
+        the relevant individual template, family template and associated metadata files. Each step will validate
+        these files to ensure that the information relating individuals, families and samples is correctly formatted.
+      </p>
+    </fragment>
+  )
+}
+
+export default Welcome
+
