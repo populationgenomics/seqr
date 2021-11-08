20ae88f4a476b63ddd0f44154806858cf4160ae0
daniaki <7043686+daniaki@users.noreply.github.com>
2021-11-08 17:10:24 +1100

    Implemented state handling for changes occurring in each form step

--- ui/pages/Project/components/DataLoadingWizardForm/DataLoadingWizardForm.jsx
+++ ui/pages/Project/components/DataLoadingWizardForm/DataLoadingWizardForm.jsx
@@ -1,31 +1,112 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import PropTypes from 'prop-types'
-import { Breadcrumb, Header } from 'semantic-ui-react'
+import { Breadcrumb } from 'semantic-ui-react'
 
 import { Centered, FormSection, FormStepButtons } from './ui'
-import { Welcome } from './steps'
+import { Welcome, FamilyMetadataUpload } from './steps'
+import { Error404 } from '../../../../shared/components/page/Errors'
 
-const FORM_STEPS = [
-  { key: 0, content: 'Welcome', link: false, active: true, component: Welcome },
-  { key: 1, content: 'Family metadata', link: false, active: false, component: () => {} },
-  { key: 2, content: 'Individual metadata', link: false, active: false, component: () => {} },
-  { key: 3, content: 'Sample mapping', link: false, active: false, component: () => {} },
-  { key: 4, content: 'Submit', link: false, active: false, component: () => {} },
+const BREADCRUMBS = [
+  { key: 0, content: 'Welcome', link: true, active: true },
+  { key: 1, content: 'Family metadata', link: true, active: false },
+  { key: 2, content: 'Individual metadata', link: true, active: false },
+  { key: 3, content: 'Sample mapping', link: true, active: false },
+  { key: 4, content: 'Review', link: true, active: false },
 ]
 
+
 const DataLoadingWizardForm = ({ project }) => {
-  // const [formState, setFormState] = useState({})
-  const [formStepIndex, setFormStepIndex] = useState(0)
-  const [formSteps, setFormSteps] = useState([...FORM_STEPS])
+  // ---- Debug ---- //
+  console.group('Project Object')
+  console.log(project)
+  console.groupEnd()
+
+  // ---- State ----- //
+  const [breadCrumbs, setBreadcrumbs] = useState(BREADCRUMBS.map((b) => { return { ...b } }))
+  const [activeFormStepIndex, setActiveFormStepIndex] = useState(0)
+  const [formSteps, setFormSteps] = useState(BREADCRUMBS.map(
+    (_, index) => {
+      return { formData: {}, isComplete: (index === 0) }
+    },
+  ))
+
+  // ---- Callbacks ----- //
+  const updateFormStep = useCallback((stepNumber, { formData, isComplete }) => {
+    setFormSteps(formSteps.map(
+      (step, index) => {
+        if (stepNumber === index) {
+          return { formData, isComplete }
+        }
+        return { ...step }
+      },
+    ))
+  }, [])
+
+  const enableReview = useCallback(() => {
+    return formSteps
+      .slice(0, formSteps.length - 1)
+      .reduce((acc, step) => acc && step.isComplete, true)
+  }, [formSteps])
+
+  const enableNext = useCallback(() => {
+    if (activeFormStepIndex === (formSteps.length - 2)) {
+      return enableReview()
+    }
+
+    return activeFormStepIndex < formSteps.length
+  }, [formSteps, activeFormStepIndex, enableReview])
+
+  const getFormStepComponent = useCallback(() => {
+    switch (activeFormStepIndex) {
+      case 0:
+        return <Welcome />
+      case 1:
+        return <FamilyMetadataUpload
+          project={project}
+          onFormChange={({ formData, isComplete }) => updateFormStep(1, { formData, isComplete })}
+        />
+      case 2:
+        return <div>{ breadCrumbs[activeFormStepIndex].content }</div>
+      case 3:
+        return <div>{ breadCrumbs[activeFormStepIndex].content }</div>
+      case 4:
+        return <div>{ breadCrumbs[activeFormStepIndex].content }</div>
+      case 5:
+        return <div>{ breadCrumbs[activeFormStepIndex].content }</div>
+      default:
+        return <Error404 />
+    }
+  }, [project, activeFormStepIndex, updateFormStep])
+
+
+  // ---- Effects ---- //
+  useEffect(() => {
+    setBreadcrumbs(breadCrumbs.map((b, index) => {
+      return { ...b, active: (index === activeFormStepIndex) }
+    }))
+  }, [activeFormStepIndex])
 
   useEffect(() => {
-    setFormSteps(
-      formSteps.map(
-        (step) => { return { ...step, active: step.key === formStepIndex } },
-      ),
-    )
-  }, [formStepIndex])
+    setBreadcrumbs(
+      breadCrumbs.map((b, index) => {
+        if (index === (breadCrumbs.length - 1)) {
+          return {
+            ...b,
+            link: enableReview(),
+            onClick: enableReview() ? () => setActiveFormStepIndex(index) : null,
+          }
+        }
 
+        return {
+          ...b,
+          link: true,
+          onClick: () => setActiveFormStepIndex(index),
+        }
+      }),
+    )
+  }, [enableReview])
+
+  // ---- Render ----- //
   return (
     <section>
       <FormSection>
@@ -33,24 +114,22 @@ const DataLoadingWizardForm = ({ project }) => {
           <Breadcrumb
             icon="right angle"
             size="large"
-            sections={formSteps}
+            sections={breadCrumbs}
           />
         </Centered>
       </FormSection>
 
       <FormSection>
-        {/* React Hook components are just functions under the hood. */}
-        {/* Get the component property from the current step and call it to add it to the DOM. */}
-        {formSteps[formStepIndex].component({ project })}
-        <Header>DEBUG</Header>
-        <pre>{JSON.stringify(project, null, 2)}</pre>
+        {getFormStepComponent()}
       </FormSection>
 
       <FormSection>
         <FormStepButtons
-          isLastStep={formStepIndex === (formSteps.length - 1)}
-          onNext={() => setFormStepIndex(Math.min(formStepIndex + 1, formSteps.length - 1))}
-          onBack={() => setFormStepIndex(Math.max(0, formStepIndex - 1))}
+          isLastStep={activeFormStepIndex === (formSteps.length - 1)}
+          onNext={() => setActiveFormStepIndex(Math.min(activeFormStepIndex + 1, formSteps.length - 1))}
+          onBack={() => setActiveFormStepIndex(Math.max(0, activeFormStepIndex - 1))}
+          enableNext={enableNext()}
+          enableSubmit={enableReview()}
           onSubmit={() => alert('Yay')}
         />
       </FormSection>
@@ -60,7 +139,7 @@ const DataLoadingWizardForm = ({ project }) => {
           <Breadcrumb
             icon="right angle"
             size="small"
-            sections={formSteps}
+            sections={breadCrumbs}
           />
         </Centered>
       </FormSection>
