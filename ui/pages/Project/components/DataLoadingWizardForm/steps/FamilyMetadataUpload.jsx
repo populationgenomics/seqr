43ef74811681ece4a16a9d47359959d771437010
daniaki <7043686+daniaki@users.noreply.github.com>
2021-11-08 16:26:01 +1100

    Stubs

---
+++ ui/pages/Project/components/DataLoadingWizardForm/steps/FamilyMetadataUpload.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// import styled from 'styled-components'
+
+const FamilyMetadataUpload = ({ project, onFormChange }) => {
+  return <div />
+}
+
+FamilyMetadataUpload.propTypes = {
+  project: PropTypes.object.isRequired,
+  onFormChange: PropTypes.func.isRequired,
+}
+
+export default FamilyMetadataUpload
