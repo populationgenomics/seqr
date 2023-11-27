1be7fe4ee3b31322db6885927db75bb2229d98c9
EddieLF <eddieformaini@gmail.com>
2023-11-27 13:58:07 +1100

    Remove custom code from deploy.yaml

--- reference_data/management/commands/utils/download_utils.py
+++ reference_data/management/commands/utils/download_utils.py
@@ -19,12 +19,12 @@ def download_file(url, to_dir=tempfile.gettempdir(), verbose=True):
     if not (url and url.startswith(("http://", "https://"))):
         raise ValueError("Invalid url: {}".format(url))
     local_file_path = os.path.join(to_dir, os.path.basename(url))
-    # remote_file_size = _get_remote_file_size(url)
-    # if os.path.isfile(local_file_path) and os.path.getsize(local_file_path) == remote_file_size:
-    #     logger.info("Re-using {} previously downloaded from {}".format(local_file_path, url))
-    #     return local_file_path
-
+    remote_file_size = _get_remote_file_size(url)
+    if os.path.isfile(local_file_path) and os.path.getsize(local_file_path) == remote_file_size:
+        logger.info("Re-using {} previously downloaded from {}".format(local_file_path, url))
+        return local_file_path
     is_gz = url.endswith(".gz")
+    # Retry download up to 10 times
     nb_tries = 10
     while True:
         try:
@@ -35,7 +35,7 @@ def download_file(url, to_dir=tempfile.gettempdir(), verbose=True):
             if nb_tries == 0:
                 raise e
             logger.warning("Connection error: {}. Retrying...".format(e))
-    # response = requests.get(url, stream=is_gz)
+
     input_iter = response if is_gz else response.iter_content()
     if verbose:
         logger.info("Downloading {} to {}".format(url, local_file_path))
@@ -51,7 +51,11 @@ def download_file(url, to_dir=tempfile.gettempdir(), verbose=True):
 
 def _get_remote_file_size(url):
     if url.startswith("http"):
-        response = requests.head(url)
+        try:
+            response = requests.head(url)
+        except ConnectionError as e:
+            logger.warning("Connection error: {}. Cannot get remote file size.".format(e))
+            return 0
         return int(response.headers.get('Content-Length', '0'))
     else:
         return 0  # file size not yet implemented for FTP and other protocols
