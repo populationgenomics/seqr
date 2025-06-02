38fa8076e02a9474d569f22e4b92b808483f7ca7
EddieLF <eddieformaini@gmail.com>
2025-06-02 14:38:58 +1000

    Display gnomAD frequencies as gnomAD v3, not v4

--- ui/shared/components/panel/variants/Frequencies.jsx
+++ ui/shared/components/panel/variants/Frequencies.jsx
@@ -191,7 +191,7 @@ const POPULATIONS = [
     field: 'gnomad_genomes',
     fieldTitle: 'gnomAD genomes',
     titleContainer: gnomadLink,
-    esVersion: 'v4',
+    esVersion: 'v3',
     conditionalQueryParams: populations => (populations.seqr ? GNOMAD_URL_INFO.queryParams : { [GENOME_VERSION_38]: 'dataset=gnomad_r4' }),
     precision: 3,
     ...GNOMAD_URL_INFO,
